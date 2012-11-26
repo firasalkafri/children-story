68cdb7bd9bd0b7a2ae51653f042334375183c160
Ahn Byung Hyun <abh0518@gmail.com>
2012-11-27 01:26:23 +0900

    이벤트 정

--- grandmom/src/com/someday/story/grandmom/MainActivity.java
+++ grandmom/src/com/someday/story/grandmom/MainActivity.java
@@ -6,7 +6,6 @@ import java.lang.ref.WeakReference;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -16,7 +15,6 @@ import android.os.Message;
 import android.os.PowerManager;
 import android.util.Log;
 import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,14 +23,15 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.someday.story.grandmom.listener.MainGestureListener;
+import com.someday.story.grandmom.listener.MySeekBarChangeListener;
 import com.someday.story.grandmom.util.AudioUtils;
 
-public class MainActivity extends Activity implements OnGestureListener {
-
+public class MainActivity extends Activity {
+    
     private static final String LOG_TAG = MainActivity.class.getName();
     
     private boolean enableBackButton = false;
@@ -42,24 +41,13 @@ public class MainActivity extends Activity implements OnGestureListener {
     private SeekBar sb;
 
     private GestureDetector gestureDetector;
-
-    private static final int SWIPE_MIN_DISTANCE = 120;
-    private static final int SWIPE_MAX_OFF_PATH = 250;
-    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
     
-    private static final int SHORT_JUMP_DISTANCE = 10 * 1000;
-    private static final int LONG_JUMP_DISTANCE = 20 * 1000;
-
-    private static final long MIN_ACTION_DELAY = 300; //�곗� 紐�� �����0.3珥�
-    private static final long ACCELETE_ACTION_DELAY = 3000; //媛����� �����3珥�
-    private static final int MIN_ACTION_X_DISTANCE = 160; //X異�理�� 諛�� 1�몄�
-    private static final int MIN_ACTION_Y_DISTANCE = 80; //Y異�理�� 諛�� 0.5�몄�
-    
+   
     public static final int CHANGE_BACKGROUND = 1;
     public static final int DISPLAY_ON_REPLAY_BTN = 2;
 
-    private static MediaPlayer mp;
-    protected AudioManager am;
+    private MediaPlayer mediaPlayer;
+    protected AudioManager audioManager;
     
     private static int[] audioTimeIndex;
     private static String[] imageIndex;
@@ -70,13 +58,13 @@ public class MainActivity extends Activity implements OnGestureListener {
     
     private Handler handler = new BackgroundChangeHandler(this);
     
-    private float firstX;
-    private float firstY;
-    private long lastActionTime;
+//    private float firstX;
+//    private float firstY;
+//    private long lastActionTime;
     private float mDensity;
 
     void handleMessage(Message msg) {
-        final long currentPosition = mp.getCurrentPosition();
+        final long currentPosition = mediaPlayer.getCurrentPosition();
         
         sb.setProgress((int)currentPosition);
         
@@ -104,26 +92,11 @@ public class MainActivity extends Activity implements OnGestureListener {
         }
     }
     
-    static class MySeekBarChangeListener implements OnSeekBarChangeListener{
-
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        }
-
-        public void onStartTrackingTouch(SeekBar seekBar) {
-            mp.pause();
-        }
-
-        public void onStopTrackingTouch(SeekBar seekBar) {
-            mp.seekTo(seekBar.getProgress());
-            mp.start();
-        }
-        
-    }
-    
     public void replay() {
-        mp.seekTo(0);
+        mediaPlayer.seekTo(0);
         sb.setProgress(0);
     }
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         
@@ -131,8 +104,8 @@ public class MainActivity extends Activity implements OnGestureListener {
         
         super.onCreate(savedInstanceState);
         
-        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        mp = getMediaPlayer(R.raw.grandma);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mediaPlayer = createMediaPlayer(R.raw.grandma);
         
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -143,7 +116,7 @@ public class MainActivity extends Activity implements OnGestureListener {
         Message m = handler.obtainMessage(CHANGE_BACKGROUND, null);
         handler.sendMessageDelayed(m, 200);
         
-        gestureDetector = new GestureDetector(this);
+        gestureDetector = new GestureDetector(new MainGestureListener(this));
         
         main = new LinearLayout(this);
         main.setBackgroundColor(Color.GRAY);
@@ -160,12 +133,12 @@ public class MainActivity extends Activity implements OnGestureListener {
         console.setTextSize(30);
         
         sb = new SeekBar(this);
-        sb.setMax(mp.getDuration());
+        sb.setMax(mediaPlayer.getDuration());
         LayoutParams sbParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
         sbParams.gravity = Gravity.BOTTOM;
         sb.setLayoutParams(sbParams);
         sb.setBackgroundColor(Color.TRANSPARENT);
-        sb.setOnSeekBarChangeListener(new MySeekBarChangeListener());
+        sb.setOnSeekBarChangeListener(new MySeekBarChangeListener(this.mediaPlayer));
         sb.setVisibility(SeekBar.INVISIBLE);
         main.addView(console);
         main.addView(sb);
@@ -184,7 +157,7 @@ public class MainActivity extends Activity implements OnGestureListener {
         
         mDensity = getResources().getDisplayMetrics().density;
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -220,7 +193,7 @@ public class MainActivity extends Activity implements OnGestureListener {
 
     @Override
     protected void onDestroy() {
-        mp.stop();
+        mediaPlayer.stop();
         this.wakeLock.release();
         super.onDestroy();
     }
@@ -230,7 +203,7 @@ public class MainActivity extends Activity implements OnGestureListener {
         return super.onPrepareOptionsMenu(menu);
     }
 
-    private MediaPlayer getMediaPlayer(int audioSource) {
+    private MediaPlayer createMediaPlayer(int audioSource) {
 		MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
 				audioSource);
 		try {
@@ -241,67 +214,42 @@ public class MainActivity extends Activity implements OnGestureListener {
 		return mediaPlayer;
     }
 
+    public MediaPlayer getMediaPlayer(){
+    	return this.mediaPlayer;
+    }
+    
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                firstX = event.getX();
-                firstY = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                toast.cancel();
-                
-                if (lastActionTime + MIN_ACTION_DELAY > System.currentTimeMillis()) break;
-                int jumpDistance;
-                
-                if(lastActionTime + ACCELETE_ACTION_DELAY > System.currentTimeMillis()) jumpDistance = SHORT_JUMP_DISTANCE;
-                else jumpDistance = LONG_JUMP_DISTANCE;
-                
-                int currentPosition = mp.getCurrentPosition();
-                
-                if (event.getX() - firstX > MIN_ACTION_X_DISTANCE) fastForward(currentPosition, jumpDistance);
-                else if (event.getX() - firstX < -MIN_ACTION_X_DISTANCE) rewind(currentPosition, jumpDistance);
-                else if (event.getY() - firstY < -MIN_ACTION_Y_DISTANCE) volumnUp();
-                else if (event.getY() - firstY > MIN_ACTION_Y_DISTANCE) volumnDown();
-                
-                lastActionTime = System.currentTimeMillis();
-                toast.show();
-                
-                break;
-        }
+//    	switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                firstX = event.getX();
+//                firstY = event.getY();
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+//                toast.cancel();
+//                
+//                if (lastActionTime + MIN_ACTION_DELAY > System.currentTimeMillis()) break;
+//                int jumpDistance;
+//                
+//                if(lastActionTime + ACCELETE_ACTION_DELAY > System.currentTimeMillis()) jumpDistance = SHORT_JUMP_DISTANCE;
+//                else jumpDistance = LONG_JUMP_DISTANCE;
+//                
+//                int currentPosition = mp.getCurrentPosition();
+//                
+//                if (event.getX() - firstX > MIN_ACTION_X_DISTANCE) fastForward(currentPosition, jumpDistance);
+//                else if (event.getX() - firstX < -MIN_ACTION_X_DISTANCE) rewind(currentPosition, jumpDistance);
+//                else if (event.getY() - firstY < -MIN_ACTION_Y_DISTANCE) volumnUp();
+//                else if (event.getY() - firstY > MIN_ACTION_Y_DISTANCE) volumnDown();
+//                
+//                lastActionTime = System.currentTimeMillis();
+//                toast.show();
+//                
+//                break;
+//        }
         return gestureDetector.onTouchEvent(event);
     }
 
-    public boolean onDown(MotionEvent arg0) {
-        return false;
-    }
     
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-		toast.cancel();
-		
-		if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-			return false;
-		int currentPosition = mp.getCurrentPosition();
-		if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-				&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
-			rewind(currentPosition, SHORT_JUMP_DISTANCE);
-		else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-				&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
-			fastForward(currentPosition, SHORT_JUMP_DISTANCE);
-		else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
-				&& Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
-			volumnUp();
-		else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
-				&& Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
-			volumnDown();
-		
-		toast.show();
-		return false;
-    }
-
-    public void onLongPress(MotionEvent ev) {
-    }
-
     @Override
     public void onBackPressed() {
         if (enableBackButton) {
@@ -312,57 +260,61 @@ public class MainActivity extends Activity implements OnGestureListener {
         Toast.makeText(getApplicationContext(), getString(R.string.text_double_back_tab), Toast.LENGTH_SHORT).show();
     }
 
-    public boolean onScroll(MotionEvent ev1, MotionEvent ev2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    public void onShowPress(MotionEvent ev) {
-    }
-
-    public boolean onSingleTapUp(MotionEvent ev) {
-        toast.cancel();
-        if (mp.isPlaying()) {
-            mp.pause();
-            console.setText(getString(R.string.text_pause));
-            toast.setText(getString(R.string.text_pause));
-        } else {
-            mp.start();
-            sb.setVisibility(SeekBar.VISIBLE);
-            console.setText(getString(R.string.text_play));
-            toast.setText(getString(R.string.text_play));
-        }
-        toast.show();
-        return false;
-    }
-
-    private void rewind(int currentPosition, int jumpDistance) {
+    public void rewind(int currentPosition, int jumpDistance) {
+    	toast.cancel();
         if (currentPosition >= jumpDistance){
-            mp.seekTo(currentPosition - jumpDistance);
+            mediaPlayer.seekTo(currentPosition - jumpDistance);
             sb.setProgress(currentPosition - jumpDistance);
         }else{
-            mp.seekTo(0);
+            mediaPlayer.seekTo(0);
             sb.setProgress(0);
         } 
         toast.setText(getString(R.string.text_rewind));
+		toast.show();
     }
     
-    private void fastForward(int currentPosition, int jumpDistance) {
-        if (currentPosition + jumpDistance < mp.getDuration()) {
-            mp.seekTo(currentPosition + jumpDistance);
+    public void fastForward(int currentPosition, int jumpDistance) {
+        if (currentPosition + jumpDistance < mediaPlayer.getDuration()) {
+        	toast.cancel();
+            mediaPlayer.seekTo(currentPosition + jumpDistance);
             sb.setProgress(currentPosition + jumpDistance);
             toast.setText(getString(R.string.text_fast_foward));
+    		toast.show();
         }
-        
     }
     
-    private void volumnUp() {
-        AudioUtils.volumeUp(am);
+    public void volumnUp() {
+    	toast.cancel();
+        AudioUtils.volumeUp(audioManager);
         toast.setText(getString(R.string.text_volume_up));
+        toast.show();
     }
     
-    private void volumnDown() {
-        AudioUtils.volumeDown(am);
+    public void volumnDown() {
+    	toast.cancel();
+        AudioUtils.volumeDown(audioManager);
         toast.setText(getString(R.string.text_volume_down));
+        toast.show();
     }
 
+    public void mediaPause(){
+    	toast.cancel();
+    	this.mediaPause();
+    	console.setText(getString(R.string.text_pause));
+    	toast.setText(getString(R.string.text_pause));
+    	toast.show();
+    }
+    
+    public void mediaStart(){
+    	toast.cancel();
+    	this.mediaStart();
+    	sb.setVisibility(SeekBar.VISIBLE);
+    	console.setText(getString(R.string.text_play));
+      	toast.setText(getString(R.string.text_play));
+      	toast.show();
+    }
+    
+    public boolean isPlaying(){
+    	return this.mediaPlayer.isPlaying();
+    }
 }
